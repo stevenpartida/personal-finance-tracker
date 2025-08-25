412ee16b9d933a80444d7dcfda5aa7d7dcee8ba4
Steven Partida <spartida0002@gmail.com>
2025-08-24 20:46:25 -0700

    Initial commit

---
+++ backend/src/test/java/com/stevenmadeit/FinanceTracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevenmadeit.FinanceTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
